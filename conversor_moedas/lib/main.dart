bd1f74c19a5a2a2b5fc17fbe4ca9a7cca2bb5bc9
zvinnicius <vinicius.goncalves@eliteconsultores.com.br>
2019-09-09 13:36:23 -0300

    Iniciando a primeira aula

--- conversor_moedas/lib/main.dart
+++ conversor_moedas/lib/main.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
+
 //minha chave de API -> 0d571128 
+const request = "https://api.hgbrasil.com/finance?key=0d571128";
+
+void main(){
+  runApp(MaterialApp(
+    home: Container(),
+  ));
+}
